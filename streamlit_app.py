91131f52402111b49ed8916d9d902a01916b6f37
simosnowflake <126288042+simosnowflake@users.noreply.github.com>
2023-02-25 16:24:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,8 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
+
 
 
 # write your own comment -what does the next line do? 
